902ae6d9c10034b136c54de831ffeca9a9960af6
tatsu <tatuo0228@icloud.com>
2018-08-03 03:00:18 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,6 +4,7 @@ class ToppagesController < ApplicationController
       @user = current_user
       @micropost = current_user.microposts.build  # form_for 用
       @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
     end
   end
 end
